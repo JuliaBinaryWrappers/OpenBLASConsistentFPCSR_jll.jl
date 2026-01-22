a143625cfe1de213cbc4462e9c4fa7886420aa06
jlbuild <juliabuildbot@gmail.com>
2026-01-22 15:14:01 +0000

    OpenBLASConsistentFPCSR_jll build 0.3.30+0

--- src/OpenBLASConsistentFPCSR_jll.jl
+++ src/OpenBLASConsistentFPCSR_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("OpenBLASConsistentFPCSR")
-JLLWrappers.@generate_main_file("OpenBLASConsistentFPCSR", UUID("6cdc7f73-28fd-5e50-80fb-958a8875b1af"))
+JLLWrappers.@generate_main_file("OpenBLASConsistentFPCSR", Base.UUID("6cdc7f73-28fd-5e50-80fb-958a8875b1af"))
 end  # module OpenBLASConsistentFPCSR_jll
